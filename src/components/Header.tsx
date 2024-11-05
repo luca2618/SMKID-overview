7e65ee59d2d5cdbb216fb2bf9dff418b692d3e33
Lucas Sylvester <lucassylvester02@gmail.com>
2024-11-05 15:18:25 +0100

    Add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,14 @@ export function Header() {
       <div className="absolute inset-0 bg-gradient-to-br from-custom-navy via-custom-slate to-custom-navy opacity-50" />
       <div className="relative max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center sm:flex-row sm:justify-center sm:space-x-8">
-          <Logo />
+          <img src="SMKID-Logo.png"></img>
           <div>
             <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
               <span className="block text-custom-blue">SMKID</span>
               <span className="block text-2xl mt-3 text-gray-300">Studieråd for Software, Matematik og Kunstig Intelligens</span>
             </h1>
             <p className="mt-6 text-xl text-gray-300">
-              Din portal til avancerede beregningsværktøjer og forskningsressourcer
+              Her finder du en oversigt over alle SM/KID's apps og ressourcer.
             </p>
           </div>
         </div>
