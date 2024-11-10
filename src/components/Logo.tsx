101d5c09fbafebe3599d64d4e4714b7e3807bad1
Lucas Sylvester <lucassylvester02@gmail.com>
2024-11-10 21:43:24 +0100

    Logo glow

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import logo from '../assets/SMKID-Logo.png';
 
 export function Logo() {
-  return <img src={logo} alt="Logo" />;
+  return <img src={logo} alt="Logo" style={{
+    width: '300px',
+    filter: 'drop-shadow(0 0 3px rgba(255, 255, 255, 0.8))',
+    borderRadius: '2px' // optional if you want rounded corners
+  }}/>;
 }
